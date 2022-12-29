5bfd8a40e11966829459061f274fdd0feb9f77f3
Paul <paul.dotterer@icloud.com>
2022-12-29 21:32:22 +0100

    added default exports for Head and Body components

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,5 @@
 // Reexport your entry components here
 export { default as CookieConsentBanner } from './cookieConsent/banner.svelte';
 export { trackingConsent, cookieConsentSeen } from './cookieConsent/bannerStores';
+export { default as TrackingBody } from './tracking/trackingBody.svelte';
+export { default as TrackingHead } from './tracking/trackingHead.svelte';
