908644adfcde58dbf7e7a7575589b4454ec32c52
Paul <paul.dotterer@icloud.com>
2022-12-29 20:37:06 +0100

    added default exports

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,3 @@
 // Reexport your entry components here
+export { default as CookieConsentBanner } from '$lib/cookieConsent/banner.svelte';
+export { trackingConsent, cookieConsentSeen } from '$lib/cookieConsent/bannerStores';
