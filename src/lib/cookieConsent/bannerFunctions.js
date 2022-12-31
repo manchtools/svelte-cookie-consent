9b9812bb3ca90ce588df2090ac174ac7d2b8fd4b
Paul <paul.dotterer@icloud.com>
2022-12-31 21:33:24 +0100

    restructured function

--- src/lib/cookieConsent/bannerFunctions.js
+++ src/lib/cookieConsent/bannerFunctions.js
@@ -1,5 +1,12 @@
 import Cookies from 'js-cookie';
-import { trackingConsent, cookieConsentSeen, advertisementConsent } from './bannerStores';
+import { get } from 'svelte/store';
+import {
+	trackingConsent,
+	cookieConsentSeen,
+	advertisementConsent,
+	allowAdvertising,
+	allowTracking
+} from './bannerStores';
 
 export function setTrackingAllowedCookie() {
 	Cookies.set('tracking-cookie-consent', 'true', { expires: 365 });
@@ -11,11 +18,11 @@ export function setTrackingDisallowedCookie() {
 }
 export function setAdvertisementAllowedCookie() {
 	Cookies.set('advertising-cookie-consent', 'true', { expires: 365 });
-	trackingConsent.set(true);
+	advertisementConsent.set(true);
 }
 export function setAdvertismentDisallowedCookie() {
 	Cookies.set('advertising-cookie-consent', 'false', { expires: 365 });
-	trackingConsent.set(false);
+	advertisementConsent.set(false);
 }
 export function setCookieConsentSeen() {
 	Cookies.set('cookie-banner-seen', 'true', { expires: 365 });
@@ -29,4 +36,43 @@ export function resetCookieBanner() {
 	trackingConsent.set(false);
 	cookieConsentSeen.set(false);
 	advertisementConsent.set(false);
+	resetSelection();
+}
+
+export function resetSelection() {
+	allowTracking.set(false);
+	allowAdvertising.set(false);
+}
+
+export function setSelectedConsent() {
+	const tmpAd = get(allowAdvertising);
+	const tmpTrack = get(allowTracking);
+	if (tmpAd === false) {
+		setAdvertismentDisallowedCookie();
+	}
+	if (tmpAd === true) {
+		setAdvertisementAllowedCookie();
+	}
+	if (tmpTrack === false) {
+		setTrackingDisallowedCookie();
+	}
+	if (tmpTrack === true) {
+		setTrackingAllowedCookie();
+	}
+	resetSelection();
+	setCookieConsentSeen();
+}
+
+export function allowAll() {
+	setCookieConsentSeen();
+	setTrackingAllowedCookie();
+	setAdvertisementAllowedCookie();
+	resetSelection();
+}
+
+export function denyAll() {
+	setCookieConsentSeen();
+	setTrackingDisallowedCookie();
+	setAdvertismentDisallowedCookie();
+	resetSelection();
 }
