8aa43c531d59f4da6afb08231e5f68fbaf6510aa
Paul <paul.dotterer@icloud.com>
2022-12-31 18:56:37 +0100

    added exports

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,11 @@
 // Reexport your entry components here
 export { default as CookieConsentBanner } from './cookieConsent/banner.svelte';
-export { trackingConsent, cookieConsentSeen } from './cookieConsent/bannerStores';
+export {
+	trackingConsent,
+	advertisementConsent,
+	cookieConsentSeen
+} from './cookieConsent/bannerStores';
 export { default as TrackingBody } from './tracking/trackingBody.svelte';
 export { default as TrackingHead } from './tracking/trackingHead.svelte';
+export { default as AdvertisingHead } from './tracking/advertisingHead.svelte';
+export { default as AdvertisingBody } from './tracking/advertisingBody.svelte';
