0455a14aa6faa1fa842d1705101e76947ed9cd5b
Paul <paul.dotterer@icloud.com>
2022-12-31 21:33:44 +0100

    added 2 new stores for intermedeate saving

--- src/lib/cookieConsent/bannerStores.js
+++ src/lib/cookieConsent/bannerStores.js
@@ -2,3 +2,5 @@ import { writable } from 'svelte/store';
 export const trackingConsent = writable(false);
 export const advertisementConsent = writable(false);
 export const cookieConsentSeen = writable();
+export const allowTracking = writable(false);
+export const allowAdvertising = writable(false);
