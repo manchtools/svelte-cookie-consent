d329a3d46fd288579e879081db637daf86c00508
Paul <paul.dotterer@icloud.com>
2023-01-01 15:14:19 +0100

    added marketing consent test to test page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,8 @@
 	import TrackingBody from '$lib/tracking/trackingBody.svelte';
 	import AdvertisementBody from '$lib/advertisement/advertisementBody.svelte';
 	import AdvertisementHead from '$lib/advertisement/advertisementHead.svelte';
+	import MarketingHead from '$lib/marketing/marketingHead.svelte';
+	import MarketingBody from '$lib/marketing/marketingBody.svelte';
 	import { resetCookieBanner } from '$lib/cookieConsent/bannerFunctions';
 </script>
 
@@ -12,8 +14,10 @@
 <Banner />
 <TrackingBody>This Body can only be seen once Tracking is accepted</TrackingBody>
 <AdvertisementBody>This Body can only be seen once Advertisement is accepted</AdvertisementBody>
+<MarketingBody>This Body can only be seen once Marketing is accepted</MarketingBody>
 <TrackingHead><title>Tracking accepted </title></TrackingHead>
 <AdvertisementHead><title>Advertising accepted </title></AdvertisementHead>
+<MarketingHead><title>Marketing accepted </title></MarketingHead>
 
 <style>
 	:global(body) {
