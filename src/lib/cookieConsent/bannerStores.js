bb544f697d1d8a1ba56a7d4487e48ec88c6d66e8
Paul <paul.dotterer@icloud.com>
2023-01-01 15:05:22 +0100

    added store for marketing consent

--- src/lib/cookieConsent/bannerStores.js
+++ src/lib/cookieConsent/bannerStores.js
@@ -1,6 +1,7 @@
 import { writable } from 'svelte/store';
 export const trackingConsent = writable(false);
 export const advertisementConsent = writable(false);
+export const marketingConsent = writable(false);
 export const cookieConsentSeen = writable();
 export const allowTracking = writable(false);
 export const allowAdvertising = writable(false);
