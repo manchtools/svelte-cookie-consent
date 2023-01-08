63572088f789e030277729e31254d99b04d7d210
Paul <paul.dotterer@icloud.com>
2023-01-08 12:00:38 +0100

    added Marketing components

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,9 +3,12 @@ export { default as CookieConsentBanner } from './cookieConsent/banner.svelte';
 export {
 	trackingConsent,
 	advertisementConsent,
+	marketingConsent,
 	cookieConsentSeen
 } from './cookieConsent/bannerStores';
 export { default as TrackingBody } from './tracking/trackingBody.svelte';
 export { default as TrackingHead } from './tracking/trackingHead.svelte';
 export { default as AdvertisementHead } from './advertisement/advertisementHead.svelte';
 export { default as AdvertisementBody } from './advertisement/advertisementBody.svelte';
+export { default as MarketingHead } from './marketing/marketingHead.svelte';
+export { default as MarketingBody } from './marketing/marketingBody.svelte';
